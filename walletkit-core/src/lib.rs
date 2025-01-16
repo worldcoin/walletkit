5f0f6c20d456bbd2983995a25870861ee0c965cc
pdtfh <149602456+pdtfh@users.noreply.github.com>
2025-01-16 23:26:49 +0100

    feat: full docs, README, contributing guidelines (#21)

--- walletkit-core/src/lib.rs
+++ walletkit-core/src/lib.rs
@@ -1,20 +1,40 @@
-#![deny(clippy::all, clippy::pedantic, clippy::nursery)]
+//! `walletkit-core` contains the basic primitives for using a World ID.
+//! Enables basic usage of a World ID to generate ZKPs using different credentials.
+#![deny(clippy::all, clippy::pedantic, clippy::nursery, missing_docs)]
 
 use strum::EnumString;
 
+/// Represents the environment in which a World ID is being presented and used.
+///
+/// Each environment uses different sources of truth for the World ID credentials.
+///
+/// More information on testing for the World ID Protocol can be found in: `https://docs.world.org/world-id/quick-start/testing`
 #[derive(Debug, Clone, PartialEq, Eq, uniffi::Object, EnumString)]
 #[strum(serialize_all = "lowercase")]
 pub enum Environment {
+    /// For testing purposes ONLY.
     Staging,
+    /// Live production environment. World ID Tree: `id.worldcoin.eth`
     Production,
 }
 
-pub mod credential_type;
-pub mod error;
-pub mod identity;
+mod credential_type;
+pub use credential_type::*;
+
+mod error;
+pub use error::*;
+
+mod identity;
+pub use identity::*;
+
+mod proof;
+pub use proof::*;
+
+mod u256;
+pub use u256::*;
+
+// private modules
 mod merkle_tree;
-pub mod proof;
 mod request;
-pub mod u256;
 
 uniffi::setup_scaffolding!("walletkit_core");
