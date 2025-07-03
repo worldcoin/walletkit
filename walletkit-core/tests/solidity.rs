68558beb2a6d0f044ce5134905205bc0d53b49f3
pd <paolodamico@users.noreply.github.com>
2025-07-03 13:30:21 -0600

    add a general world id test proof

--- walletkit-core/tests/solidity.rs
+++ walletkit-core/tests/solidity.rs
@@ -149,3 +149,69 @@ async fn test_address_book_proof_verification_on_chain() {
             )
     );
 }
+
+sol!(
+    /// The World ID Address Book allows verifying wallet addresses using a World ID for a period of time.
+    ///
+    /// Reference: <https://github.com/worldcoin/world-id-contracts/blob/main/src/WorldIDRouterImplV1.sol#L342>
+    ///
+    /// Reference: <https://github.com/worldcoin/world-id-contracts/blob/main/src/interfaces/IWorldID.sol#L9>
+    #[sol(rpc)]
+    #[sol(rename_all = "camelcase")]
+    interface IWorldIDRouter {
+        function verifyProof(
+            uint256 root,
+            uint256 groupId,
+            uint256 signal_hash,
+            uint256 nullifier_hash,
+            uint256 external_nullifier_hash,
+            uint256[8] calldata proof
+        ) external view;
+    }
+);
+
+#[tokio::test]
+async fn test_verify_simple_world_id_proof_on_chain() {
+    // set up a World Chain Sepolia fork with the `WorldIDRouter` contract (proxy for `IWorldID`).
+    let anvil = setup_anvil();
+    let owner_signer = PrivateKeySigner::random();
+    let owner = owner_signer.address();
+    let provider = ProviderBuilder::new()
+        .wallet(owner_signer)
+        .connect_http(anvil.endpoint_url());
+    provider
+        .anvil_set_balance(owner, U256::from(1e19))
+        .await
+        .unwrap();
+
+    // The World Chain Sepolia (4801) World ID Router contract
+    // Reference: <https://docs.world.org/world-id/reference/contract-deployments>
+    let contract_address = address!("0x57f928158C3EE7CDad1e4D8642503c4D0201f611");
+    let world_id_contract = IWorldIDRouter::new(contract_address, &provider);
+
+    // initialize a World ID and generate the relevant proof context
+    let world_id = WorldId::new(b"not_a_real_secret", &Environment::Staging);
+
+    let proof_context = ProofContext::new(
+        "app_staging_509648994ab005fe79c4ddd0449606ca",
+        None,
+        Some("my_signal".to_string()),
+        CredentialType::Orb,
+    );
+
+    let proof = world_id.generate_proof(&proof_context).await.unwrap();
+
+    world_id_contract
+        .verifyProof(
+            proof.merkle_root.into(),
+            U256::from(1), // 1 is the `groupId` for the Orb verification group
+            proof_context.signal_hash.into(),
+            proof.nullifier_hash.into(),
+            proof_context.external_nullifier.into(),
+            proof.raw_proof.flatten(),
+        )
+        .from(owner)
+        .call()
+        .await
+        .unwrap();
+}
