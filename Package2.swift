e913919469c981faa5c8d9a79b93d97e6a5e6a09
Luke Mann <luke@mann.xyz>
2026-01-16 10:55:36 -0800

    Add activity feed API2

---
+++ Package2.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// The package must be installed from github.com/worldcoin/walletkit-swift
+
+import PackageDescription
+
+let package = Package(
+    name: "WalletKit",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "WalletKit",
+            targets: ["WalletKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "WalletKit",
+            dependencies: ["walletkit_FFI"]
+        ),
+        .binaryTarget(name: "walletkit_FFI", path: "WalletKit.xcframework")
+    ]
+)
