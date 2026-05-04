2ebf8684e6543066df22d10f697f14f32e82df50
dzejkop <jakubtrad@gmail.com>
2026-05-04 16:41:35 +0200

    wasm build support

---
+++ nix/wasm.nix
@@ -0,0 +1,26 @@
+{ pkgs }:
+let
+  rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
+  llvm = pkgs.llvmPackages;
+in
+pkgs.mkShell {
+  packages = [
+    rustToolchain
+    llvm.clang-unwrapped
+    llvm.bintools-unwrapped
+    pkgs.curl
+    pkgs.git
+  ];
+
+  # Use unwrapped clang: cc-wrapper injects host hardening flags that are invalid for wasm.
+  CC_wasm32_unknown_unknown = "${llvm.clang-unwrapped}/bin/clang";
+  AR_wasm32_unknown_unknown = "${llvm.bintools-unwrapped}/bin/llvm-ar";
+
+  shellHook = ''
+    echo "WalletKit wasm dev shell"
+    echo "  target: wasm32-unknown-unknown"
+    echo "  clang: $CC_wasm32_unknown_unknown"
+    echo ""
+    echo "Build with: cargo build -p walletkit --target wasm32-unknown-unknown"
+  '';
+}
