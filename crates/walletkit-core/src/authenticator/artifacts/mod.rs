7e7ba28087e7cd8c6e8bb8d18268e99a4e77179a
Dzejkop <jakubtrad@gmail.com>
2026-08-04 12:00:40 +0200

    feature gate embedded artifact sources

--- crates/walletkit-core/src/authenticator/artifacts/mod.rs
+++ crates/walletkit-core/src/authenticator/artifacts/mod.rs
@@ -8,6 +8,9 @@ use world_id_core::artifacts::ZkArtifactSource;
 pub trait WalletKitZkArtifactSource: ZkArtifactSource + Send {}
 impl<T> WalletKitZkArtifactSource for T where T: ZkArtifactSource + Send {}
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(not(target_arch = "wasm32"), feature = "embed-zkeys"))]
 pub mod caching;
+
+/// Sources backed by ZK artifacts embedded in the binary.
+#[cfg(feature = "embed-zkeys")]
 pub mod embedded;
